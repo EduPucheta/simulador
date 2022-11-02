6403fcc24d3534b9eeca4d39e59c13c8b53fccb3
EduPucheta <43687004+EduPucheta@users.noreply.github.com>
2022-11-02 10:34:57 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -71,9 +71,43 @@ function myFunction(e) {
       document.getElementById('mensajederesultado4').textContent =  "Te conviene en contado."
     }
     let myString = JSON.stringify(obj);
-    document.getElementById("mensajederesultado5").innerHTML = myString;
+    // document.getElementById("mensajederesultado5").innerHTML = myString;
     document.getElementById('resultado__detalle').textContent =  "El cálculo asume que los ingresos mensuales aumentan a la par que la inflación. " + "La inflación estimada mensual para este cálculo es " + rate +"%.";
+    document.getElementById('title_grafica').textContent =  "Valor actual de cada cuota"
 
+    // Obtener una referencia al elemento canvas del DOM
+const $grafica = document.querySelector("#grafica");
+// Las etiquetas son las que van en el eje X. 
+const etiquetas = arr1
+// Podemos tener varios conjuntos de datos. Comencemos con uno
+const datosVentas2020 = {
+    label: "Valor actual",
+    data: arr2, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+new Chart($grafica, {
+    type: 'bar',// Tipo de gráfica
+    data: {
+        labels: etiquetas,
+        datasets: [
+            datosVentas2020,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+    }
+});
+  
+  
   }
 
 
