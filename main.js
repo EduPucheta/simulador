b6dd27c5581aa8e7e756093ef2055a9e3f2ce22d
EduPucheta <43687004+EduPucheta@users.noreply.github.com>
2022-11-14 12:49:56 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
-var rate = 6.2 // Esta variable es el valor de la inflación mensual//
+// Define el número de sesión 
+
+var visits = Number(localStorage.getItem('visitCount'));
+var current = Boolean(sessionStorage.getItem('session'));
+if (!current) {
+  visits+=1;
+}
+localStorage.setItem('visitCount', visits);
+sessionStorage.setItem('session', true);
+
+
+
+
+// Modal de bienvenida
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Muestra el modal de bienvenida si es la primera sesión
+
+if(localStorage.getItem('visitCount')==0){
+  modal.style.display = "block";
+}
+
+// Inflación mensual tomada por defecto.
+var rate = 6.2 
 
 // Función para agregar clases a los botones seleccionados
 var  buttonArray = document.querySelectorAll('.cuotas');
